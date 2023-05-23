631d4c9f2e9caf392ad508e48bfaef10902df452
Daniela Castro <dgonzalezcs@outlook.com>
2023-05-23 13:49:33 -0600

    counter and search bar work

--- src/App.js
+++ src/App.js
@@ -14,17 +14,38 @@ const defaultToDos = [
   {text: 'Interview', completed: true},
   {text: 'Finish React.js course', completed: true},
   {text: 'Install new MacOS', completed: true},
+  {text: 'Learn states and hooks', completed: true},
 ]
 
 function App() {
+  const [ToDos, setToDos] = React.useState
+  (defaultToDos)
+  const [searchValue, setSearchValue] = React.
+  useState('');
+
+  const completedToDos = ToDos.filter(ToDo => !!ToDo.completed).length;
+  const totalToDos = ToDos.length;
+
+  const searchedToDos = ToDos.filter( //estado derivado
+    (ToDo) => {
+     return ToDo.text.toLowerCase().includes(searchValue)
+    }
+  );
+
+  console.log('You searched ' + searchValue)
   return (
     <React.Fragment>
 
-      <ToDoCounter completed={16} total={25} />
-      <ToDoSearch/>
+      <ToDoCounter 
+      completed={completedToDos} 
+      total={totalToDos} />
+
+      <ToDoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}/>
 
       <ToDoList>
-        {defaultToDos.map(ToDo => (
+        {searchedToDos.map(ToDo => (
           <ToDoItem 
           key={ToDo.text} 
           text={ToDo.text}
--- src/ToDoSearch.js
+++ src/ToDoSearch.js
@@ -4,10 +4,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
-const ToDoSearch = () => {
+function ToDoSearch ({
+  searchValue,
+  setSearchValue
+}) {
+
   return (
     <div>
-      <input type="text" className="todo-search-input" placeholder="Meeting with the team @ 5" />
+      <input 
+      type="text" 
+      className="todo-search-input" 
+      placeholder="Meeting with the team @ 5" 
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value)
+      }}
+      />
       <FontAwesomeIcon icon={faSearch} className="todo-search-icon" />
     </div>
   );
